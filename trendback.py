4645ea83ad1193cf3deb2907c37cfbf6b5167c3c
ethanbsung <ebsung@umich.edu>
2024-12-03 20:54:18 -0500

    trend

---
+++ trendback.py
@@ -0,0 +1,25 @@
+import dontshare as d
+import ccxt
+import pandas as pd
+import time
+from datetime import datetime
+
+'''
+SYMBOLS TO TRADE:
+BTC/USD
+ETH/USD
+XRP/USD
+BNB/USD
+ADA/USD
+AVAX/USD
+LINK/USD
+DOT/USD
+UNI/USD
+LTC/USD
+ICP/USD
+ETC/USD
+
+
+MAYBE:
+NEAR/USD
+'''
