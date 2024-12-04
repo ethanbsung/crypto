f66c63339179ebd5293549838f6a164fe099df2d
ethanbsung <ebsung@umich.edu>
2024-12-03 23:28:33 -0500

    riskmanager

--- riskmanager/positions.py
+++ riskmanager/positions.py
@@ -1,8 +1,12 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import ccxt
 import time
 import dontshare as d
 import logging
 from datetime import datetime
+
 
 # Set up logging
 logging.basicConfig(
