28e4ef64bec997d259c8e4870735be8baf249792
ethanbsung <ebsung@umich.edu>
2024-12-05 17:40:50 -0500

    meanrev

--- meanrevback.py
+++ meanrevback.py
@@ -1,16 +1,25 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import ccxt
 import pandas as pd
 import dontshare as d
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
+import logging
+import requests
+import json
 
-kraken = ccxt.kraken({
+kristen = ccxt.kraken({
     'apiKey': d.kraken_api_key,
     'secret': d.kraken_secret_key,
     'enableRateLimit': True,
 })
+
 '''
 SYMBOLS TO TRADE:
+WIF/USD
+POPCAT/USD
 SOL/USD
 TON/USD
 PEPE/USD
@@ -18,17 +27,14 @@ CRO/USD
 TAO/USD
 KAS/USD
 TIA/USD
-WIF/USD
 BONK/USD
 FLOKI/USD
-POPCAT/USD
 
 MAYBE:
 JUP/USD
 NEAR/USD
 RENDER/USD
 PYTH/USD
-
 '''
 symbols_data = {
     'WIF/USD': {
@@ -48,4 +54,5 @@ ranges = get_ranges(symbol)
 buy_range = ranges['buy_range']
 sell_range = ranges['sell_range']
 
+print(f"{symbol} - Buy range: {buy_range}, Sell range: {sell_range}")
 
